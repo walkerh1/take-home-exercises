20db1179f5511f9751f78c6fbe2f0cc92eaabc5b
Hugo <hugowalkersmith@gmail.com>
2021-11-24 22:07:28 +1100

    first commit - both questions mostly done.

---
+++ q1.py
@@ -0,0 +1,65 @@
+# q1 Veyor take-home.
+
+# Note: it is more pythonic to replace the custom comparator and
+# cmp_to_key below with the following line:
+
+    # array.sort(key=lambda x: (frequencies[x], x))
+
+# This line works because python tuples have an order defined on them:
+# given two tuples, python first compares the elements in position i,
+# and if they're equal, it then compares the elements in position i+1,
+# and so on. The reason I went with a custom_comparator is because it
+# shows more explicitly the logic of the sorting algorithm.
+
+
+from collections import defaultdict
+from functools import cmp_to_key
+
+
+def custom_sort(array):
+    """
+    Implements custom sort algorithm.
+    """
+    # Get frequency of each number in array - O(n) time
+    frequencies = defaultdict(int)
+    for number in array:
+        frequencies[number] += 1
+    
+    # This auxiliary function swaps an element with the element to its right
+    # if it has a higher frequency; and if it has the same frequency, swaps
+    # it only if it has a greater value.
+    def custom_comparator(num1, num2):
+        freq1 = frequencies[num1]
+        freq2 = frequencies[num2]
+        if (freq1 == freq2):
+            return num1 - num2
+        else:
+            return freq1 - freq2
+    
+    # Creates and returns sorted version of list in O(nlogn) time.
+    return sorted(array, key=cmp_to_key(custom_comparator))
+
+
+def custom_sort_and_print(array):
+    """
+    Calls custom_sort on given array and prints results.
+    """
+    sorted_list = custom_sort(array)
+    print('')
+    for number in sorted_list:
+        print(number)
+
+
+if __name__ == '__main__':
+    try:
+        n = int(input())
+        array = []
+        i = 0
+        while i < n:
+            next_number = int(input())
+            array.append(next_number)
+            i += 1
+        custom_sort_and_print(array)
+    except ValueError:
+        print("Please input integers only.")
+        exit(1)
---
+++ q2.py
@@ -0,0 +1,33 @@
+# q2 Veyor take-home
+# See README.md for why this algorithm is correct.
+
+def max_money(n, k):
+    """
+    Finds the maximum money that can be received for any 
+    number of classmates, n, and any unlucky number, k.
+    """
+    mod = 10**9 + 7
+    max_dollars = (n * (n+1)) / 2
+    cum_sum = 0
+    i = 1
+    while i <= n:
+        cum_sum += i
+        if cum_sum == k:
+            return int((max_dollars - 1) % mod)
+        i += 1
+    return int(max_dollars % mod)
+ 
+
+if __name__ == '__main__':
+    try:
+        n = int(input())
+        k = int(input())
+    except ValueError:
+        print("Please input integers only.")
+        exit(1)    
+    else:
+        print(max_money(n, k))
+    
+    # print(max_money(2, 2))  # 3
+    # print(max_money(2, 1))  # 2
+    # print(max_money(3, 3))  # 5
